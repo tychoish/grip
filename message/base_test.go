803cc96741ea83bfbce55817b13468e58afb6e9b
tycho garen <garen@tychoish.com>
2022-06-26 17:57:50 -0400

    message: refactor test

--- message/base_test.go
+++ message/base_test.go
@@ -2,57 +2,96 @@ package message
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/suite"
 )
 
-type ComposerBaseSuite struct {
-	base *Base
-	suite.Suite
+func TestCollectWorksWithUnsetPids(t *testing.T) {
+	base := &Base{}
+	if !base.IsZero() {
+		t.Fatal("base must be zero on init")
+	}
+	if base.Hostname != "" {
+		t.Error("values should be equal")
+	}
+	base.Pid = 0
+	if err := base.Collect(); err != nil {
+		t.Fatal(err)
+	}
+	if base.Hostname == "" {
+		t.Error("hostname should be populated")
+	}
 }
 
-func TestComposerBaseSuite(t *testing.T) {
-	suite.Run(t, new(ComposerBaseSuite))
+func TestCollectNoopsIfPidIsSet(t *testing.T) {
+	base := &Base{}
+	if !base.IsZero() {
+		t.Fatal("base must be zero on init")
+	}
+	if base.Hostname != "" {
+		t.Error("values should be equal")
+	}
+	base.Pid = 1
+	if err := base.Collect(); err != nil {
+		t.Fatal(err)
+	}
+	if base.Hostname != "" {
+		t.Error("values should be equal")
+	}
 }
 
-func (s *ComposerBaseSuite) SetupTest() {
-	s.base = &Base{}
-	s.True(s.base.IsZero())
+func TestAnnotateAddsFields(t *testing.T) {
+	base := &Base{}
+	if !base.IsZero() {
+		t.Fatal("base must be zero on init")
+	}
+	if base.Context != nil {
+		t.Fatal("context should not be populated yet")
+	}
+	if err := base.Annotate("k", "foo"); err != nil {
+		t.Fatal(err)
+	}
+	if base.Context == nil {
+		t.Error("context should be populated")
+	}
 }
 
-func (s *ComposerBaseSuite) TestCollectWorksWithUnsetPids() {
-	s.Equal("", s.base.Hostname)
-	s.base.Pid = 0
-	s.NoError(s.base.Collect())
-	s.NotEqual("", s.base.Hostname)
-
+func TestAnnotateErrorsForSameValue(t *testing.T) {
+	base := &Base{}
+	if !base.IsZero() {
+		t.Fatal("base must be zero on init")
+	}
+	if err := base.Annotate("k", "foo"); err != nil {
+		t.Fatal(err)
+	}
+	if err := base.Annotate("k", "foo"); err == nil {
+		t.Error("error should not be nil")
+	}
+	if base.Context["k"] != "foo" {
+		t.Error("values should be equal")
+	}
 }
 
-func (s *ComposerBaseSuite) TestCollectNoopsIfPidIsSet() {
-	s.Equal("", s.base.Hostname)
-	s.base.Pid = 1
-	s.NoError(s.base.Collect())
-	s.Equal("", s.base.Hostname)
-}
+func TestAnnotateMultipleValues(t *testing.T) {
+	base := &Base{}
+	if !base.IsZero() {
+		t.Fatal("base must be zero on init")
+	}
+	if base.Structured() {
+		t.Fatal("should not be structured yet")
 
-func (s *ComposerBaseSuite) TestAnnotateAddsFields() {
-	s.Nil(s.base.Context)
-	s.NoError(s.base.Annotate("k", "foo"))
-	s.NotNil(s.base.Context)
-}
-
-func (s *ComposerBaseSuite) TestAnnotateErrorsForSameValue() {
-	s.NoError(s.base.Annotate("k", "foo"))
-	s.Error(s.base.Annotate("k", "foo"))
-
-	s.Equal("foo", s.base.Context["k"])
-}
-
-func (s *ComposerBaseSuite) TestAnnotateMultipleValues() {
-	s.False(s.base.Structured())
-	s.NoError(s.base.Annotate("kOne", "foo"))
-	s.NoError(s.base.Annotate("kTwo", "foo"))
-	s.Equal("foo", s.base.Context["kOne"])
-	s.Equal("foo", s.base.Context["kTwo"])
-	s.True(s.base.Structured())
+	}
+	if err := base.Annotate("kOne", "foo"); err != nil {
+		t.Fatal(err)
+	}
+	if err := base.Annotate("kTwo", "foo"); err != nil {
+		t.Fatal(err)
+	}
+	if base.Context["kOne"] != "foo" {
+		t.Error("values should be equal")
+	}
+	if base.Context["kTwo"] != "foo" {
+		t.Error("values should be equal")
+	}
+	if !base.Structured() {
+		t.Fatal("should be structured")
+	}
 }
