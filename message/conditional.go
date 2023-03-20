765cc7131135513dc72d5ac82577ca1d3901f598
tycho garen <garen@tychoish.com>
2023-03-19 20:24:09 -0400

    message: conditional cleanup

--- message/conditional.go
+++ message/conditional.go
@@ -12,9 +12,7 @@ type condComposer struct {
 // When returns a conditional message that is only logged if the
 // condition is bool. Converts the second argument to a composer, if
 // needed, using the same rules that the logging methods use.
-func When(cond bool, m any) Composer {
-	return &condComposer{cond: cond, msg: ConvertWithPriority(level.Priority(0), m)}
-}
+func When(cond bool, m any) Composer { return &condComposer{cond: cond, msg: Convert(m)} }
 
 // Whenf returns a conditional message that is only logged if the
 // condition is bool, and creates a sprintf-style message, which will
@@ -34,9 +32,7 @@ func Whenln(cond bool, args ...any) Composer {
 // condition is bool, and creates a string message that will only log
 // when the message content is not the empty string. Use this for a
 // more strongly-typed conditional logging message.
-func WhenMsg(cond bool, m string) Composer {
-	return &condComposer{cond: cond, msg: MakeString(m)}
-}
+func WhenMsg(cond bool, m string) Composer { return &condComposer{cond: cond, msg: MakeString(m)} }
 
 func (c *condComposer) String() string                     { return c.msg.String() }
 func (c *condComposer) Raw() any                           { return c.msg.Raw() }
@@ -44,10 +40,4 @@ func (c *condComposer) Priority() level.Priority           { return c.msg.Priori
 func (c *condComposer) SetPriority(p level.Priority) error { return c.msg.SetPriority(p) }
 func (c *condComposer) Annotate(k string, v any) error     { return c.msg.Annotate(k, v) }
 func (c *condComposer) Structured() bool                   { return c.msg.Structured() }
-func (c *condComposer) Loggable() bool {
-	if c.cond {
-		return c.msg.Loggable()
-	}
-
-	return false
-}
+func (c *condComposer) Loggable() bool                     { return c.cond && c.msg.Loggable() }
