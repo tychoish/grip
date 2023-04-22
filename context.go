96e5119462c783819fb12321dcd908050074b929
tycho garen <garen@tychoish.com>
2023-04-22 11:17:55 -0400

    context: docs and cleanup

--- context.go
+++ context.go
@@ -6,7 +6,7 @@ type ctxKey string
 
 const defaultContextKey ctxKey = "__GRIP_STD_LOGGER"
 
-// WithLogger attaches a Logger instance to the context
+// WithLogger attaches a Logger instance to the context.
 func WithLogger(ctx context.Context, logger Logger) context.Context {
 	return WithContextLogger(ctx, logger, string(defaultContextKey))
 }
@@ -14,10 +14,10 @@ func WithLogger(ctx context.Context, logger Logger) context.Context {
 // Context resolves a logger from the given context, and if one does
 // not exist (or the context is nil), produces the global Logger
 // instance.
-func Context(ctx context.Context) Logger { return ContextLoger(ctx, string(defaultContextKey)) }
+func Context(ctx context.Context) Logger { return ContextLogger(ctx, string(defaultContextKey)) }
 
 // WithContextLogger attaches a logger with a specific name to the
-// context to the context. Your package should wrap this and use
+// context. Your package should wrap this and use
 // constants for logger names. In most cases, WithLogger to set a
 // default logger, or even just using the standard Context is
 // preferable.
@@ -25,10 +25,10 @@ func WithContextLogger(ctx context.Context, logger Logger, name string) context.
 	return context.WithValue(ctx, ctxKey(name), logger)
 }
 
-// ContextLoger produces a logger stored in the context by a given
+// ContextLogger produces a logger stored in the context by a given
 // name. If such a context is not stored the standard/default logger
 // is returned.
-func ContextLoger(ctx context.Context, name string) Logger {
+func ContextLogger(ctx context.Context, name string) Logger {
 	if ctx == nil {
 		return std
 	}
